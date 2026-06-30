bc90b49e31273d11dcf12c833ee41a43dc838a55
Amaresh-saravanan <amareshsaravanan2617@gmail.com>
2026-06-30 23:43:44 +0530

    fix: Correct routing - Landing on / and Hub on /hub

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
+import Landing from './pages/Landing'
 import Hub from './pages/Hub'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Hub />} />
-        <Route path="/hub" element={<Navigate to="/" replace />} />
+        <Route path="/" element={<Landing />} />
+        <Route path="/hub" element={<Hub />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
